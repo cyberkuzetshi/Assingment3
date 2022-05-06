c68c1b7774bfad0fbc37ee7a3af8840f9d2a657d
AITU homework <https://github.com/cyberkuzetshi/Aitu.git>
2022-05-07 00:22:04 +0600

    with love <3

--- src/com/company/MyHashTable.java
+++ src/com/company/MyHashTable.java
@@ -1,5 +1,14 @@
 package com.company;
 
-public class MyHashTable {
+public class MyHashTable <T extends Comparable<T>>{
+    public class Item implements Comparable<T>{
+        T data;
+        int key;
 
+        @Override
+        public int compareTo(T o) {
+            return 0;
+        }
+        int size = 0;
+    }
 }
